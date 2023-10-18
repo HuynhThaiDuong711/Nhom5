71d5ab40d96691f8f0be00300bc9299ee1f27e39
HoangKha15012003 <nguyenhoangkhalop97@gmail.com>
2023-10-18 21:53:36 +0700

    kha

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,63 @@ class IconButtonExampleApp extends StatelessWidget {
                 border: Border.all(color: Colors.black),
                 borderRadius: BorderRadius.circular(10.0), 
               ),
-
+              child: Column(
+            children: [
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.start,
+              //   crossAxisAlignment: CrossAxisAlignment.start,
+                
+              //   children:<Widget>[ Container(  
+              //     margin: const EdgeInsets.all(50.0),
+              //     color: Colors.red,
+              //     height: 48.0,
+                  
+              //     child: Row(
+              //       children: const [Text('Hồ sơ cá nhân', style: TextStyle(color: Colors.white, fontSize: 20),)],
+              //     ),)]
+              //   ),
+              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                     children: const [
+                        CircleAvatar(
+                      backgroundImage: AssetImage(''),
+                      radius: 100,)]
+                  ),
+                  Column(
+                     mainAxisAlignment: MainAxisAlignment.start,
+                        children: const [
+                          Text('Full Name:-----------------\nUsername:-----------------\nDay of Birth:-----------------\nEmail:-----------------\nNumberPhone:---------------',textAlign: TextAlign.justify,)
+                        ],
+                  ),
+                  Column(
+                    children: [
+                          Row(
+                            children:<Widget>[
+                              Container(
+                                decoration: BoxDecoration (
+                                    borderRadius: new BorderRadius.all(new Radius.circular(10.0)),
+                                    color: Colors.red
+                                ),
+                                padding: new EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+                                child: Text (
+                                    "sửa thông tin",
+                                    style: TextStyle(
+                                        color: Colors.blue[500],
+                                        fontWeight: FontWeight.w900
+                                    )
+                                ),
+                              )
+                            ]
+                          )
+                        ],
+                  )
+                ],
+              )
+            ],
+          )
               
             ),
 
